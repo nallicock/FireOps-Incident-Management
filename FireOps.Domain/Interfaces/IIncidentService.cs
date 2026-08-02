dd368ee7aad916670b4702a54de95ffe77b5ef28
nallicock <nicholasallicock@outlook.com>
2026-08-02 19:28:35 -0400

    begin adding Service business logic

---
+++ FireOps.Domain/Interfaces/IIncidentService.cs
@@ -0,0 +1,16 @@
+﻿using FireOps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireOps.Domain.Interfaces
+{
+    public interface IIncidentService
+    {
+        Task<IEnumerable<Incident>> GetAllIncsAsync();
+        Task<Incident?> GetByIdAsync(int id);
+        Task<Incident> CreateAsync(Incident incident);
+        Task UpdateAsync(Incident incident);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ FireOps.Infrastructure/Services/IncidentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FireOps.Domain.Interfaces;
+
+namespace FireOps.Infrastructure.Services
+{
+    public class IncidentService : IIncidentService
+    {
+        private readonly IIncidentRepository _repository;
+
+        public IncidentService(IIncidentRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
