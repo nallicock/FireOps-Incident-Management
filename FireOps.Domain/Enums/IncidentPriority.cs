07f463c3e1a3990bbad68ee40476f9827001c195
nallicock <nicholasallicock@outlook.com>
2026-08-02 14:28:28 -0400

    added enums to business logic

---
+++ FireOps.Domain/Enums/IncidentPriority.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireOps.Domain.Enums
+{
+    public enum IncidentPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+
+    public enum IncidentStatus
+    {
+        New = 1,
+        Assigned = 2,
+        EnRoute = 3,
+        OnScene = 4,
+        Resolved = 5,
+        Closed = 6
+    }
+}
