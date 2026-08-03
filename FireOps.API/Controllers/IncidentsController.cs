e017fd79d4182dd2eb615a7d56a7c9d3c1caaf0b
nallicock <nicholasallicock@outlook.com>
2026-08-03 11:02:42 -0400

    GET HTTP Method completed

---
+++ FireOps.API/Controllers/IncidentsController.cs
@@ -0,0 +1,25 @@
+﻿using FireOps.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FireOps.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IncidentsController : ControllerBase
+    {
+        private readonly IIncidentService _incidentService;
+
+        public IncidentsController(IIncidentService incidentService)
+        {
+            _incidentService = incidentService;
+        }
+
+        //GET
+        public async Task<IActionResult> GetAll()
+        {
+            var incidents = await _incidentService.GetAllIncsAsync();
+
+            return Ok(incidents);
+        }
+    }
+}
