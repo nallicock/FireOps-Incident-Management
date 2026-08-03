6f6439eeb7f509c6ad8c77ce0dccf4b567c07dd5
nallicock <nicholasallicock@outlook.com>
2026-08-03 10:44:33 -0400

    added more service logic

--- FireOps.Infrastructure/Services/IncidentService.cs
+++ FireOps.Infrastructure/Services/IncidentService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using FireOps.Domain.Interfaces;
+using FireOps.Domain.Entities;
 
 namespace FireOps.Infrastructure.Services
 {
@@ -12,6 +13,34 @@ namespace FireOps.Infrastructure.Services
         public IncidentService(IIncidentRepository repository)
         {
             _repository = repository;
+        }
+
+        public async Task<IEnumerable<Incident>> GetAllIncsAsync()
+        {
+            return await _repository.GetAllIncsAsync();
+        }
+
+        public async Task<Incident?> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task<Incident> CreateAsync(Incident incident)
+        {
+            incident.CreatedAt = DateTime.UtcNow;
+            incident.Status = Domain.Enums.IncidentStatus.New;
+            await _repository.AddAsync(incident);
+            return incident;
+        }
+
+        public async Task UpdateAsync(Incident incident)
+        {
+            await _repository.UpdateAsync(incident);
+        }
+
+        public async Task DeleteSync(int id)
+        {
+            await _repository.DeleteAsync(id);
         }
     }
 }
